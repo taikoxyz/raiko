0cf19f5b4d8433dd4f7813702724213a401b61cf
john xu <john@taiko.xyz>
2023-10-26 03:04:06 +0000

    feat: run serve in host

--- raiko-host/src/main.rs
+++ raiko-host/src/main.rs
@@ -13,17 +13,10 @@
 // limitations under the License.
 
 mod prover;
-
-use std::fmt::Debug;
-
 use anyhow::Result;
 use clap::Parser;
-use ethers_core::types::Transaction as EthersTransaction;
-use serde::{Deserialize, Serialize};
-use zeth_lib::{
-    block_builder::{EthereumStrategyBundle, NetworkStrategyBundle},
-    consts::{ChainSpec, ETH_MAINNET_CHAIN_SPEC},
-};
+use prover::server::serve;
+use std::fmt::Debug;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -41,10 +34,6 @@ struct Args {
     /// The guests path
     /// [default: raiko-host/guests]
     guest: Option<String>,
-}
-
-fn cache_file_path(cache_path: &String, network: &str, block_no: u64, ext: &str) -> String {
-    format!("{}/{}/{}.{}", cache_path, network, block_no, ext)
 }
 
 // Prerequisites:
@@ -65,29 +54,6 @@ fn cache_file_path(cache_path: &String, network: &str, block_no: u64, ext: &str)
 async fn main() -> Result<()> {
     env_logger::init();
     let args = Args::parse();
-
-    run_with_bundle::<EthereumStrategyBundle>(args, ETH_MAINNET_CHAIN_SPEC.clone()).await
-}
-
-async fn run_with_bundle<N: NetworkStrategyBundle>(args: Args, chain_spec: ChainSpec) -> Result<()>
-where
-    N::TxEssence: 'static + Send + TryFrom<EthersTransaction> + Serialize + Deserialize<'static>,
-    <N::TxEssence as TryFrom<EthersTransaction>>::Error: Debug,
-    <N::Database as revm::primitives::db::Database>::Error: Debug,
-{
-    // Fetch all of the initial data
-    let rpc_cache = args
-        .cache
-        .as_ref()
-        .map(|dir| cache_file_path(dir, "taiko", args.block_no, "json.gz"));
-
-    let init_spec = chain_spec.clone();
-    // let _protocol_instance = protocol_instance.clone();
-    let _init = tokio::task::spawn_blocking(move || {
-        zeth_lib::host::get_initial_data::<N>(init_spec, rpc_cache, args.rpc_url, args.block_no)
-            .expect("Could not init")
-    })
-    .await?;
-
+    serve(&args.bind, &args.guest.unwrap(), &args.cache.unwrap()).await?;
     Ok(())
 }
--- raiko-host/src/prover/error.rs
+++ raiko-host/src/prover/error.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+#[derive(Debug)]
 pub enum Error {
     Io(std::io::Error),
     Anyhow(anyhow::Error),
