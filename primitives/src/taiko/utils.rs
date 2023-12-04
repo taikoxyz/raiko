bc3bba4099d19c680bd68311df2070e49e5aa440
john xu <john@taiko.xyz>
2023-12-04 02:45:48 +0000

    Fix byte array copy in string_to_bytes32 function

--- primitives/src/taiko/utils.rs
+++ primitives/src/taiko/utils.rs
@@ -1,5 +1,6 @@
 pub fn string_to_bytes32(input: &[u8]) -> [u8; 32] {
     let mut bytes = [0u8; 32];
-    bytes.copy_from_slice(input);
+    let len = min(input.len(), 32);
+    bytes[..len].copy_from_slice(&input[..len]);
     bytes
 }
