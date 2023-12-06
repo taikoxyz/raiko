6e28dd6483fae67e82d189e31d57a9c9e1916d79
john xu <john@taiko.xyz>
2023-12-06 08:42:26 +0000

    Add feature flag for path_file_prefix

--- raiko-host/src/main.rs
+++ raiko-host/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(path_file_prefix)]
 // Copyright 2023 RISC Zero, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
--- raiko-host/src/prover/utils.rs
+++ raiko-host/src/prover/utils.rs
@@ -2,9 +2,19 @@ use super::consts::RAIKO_GUEST_EXECUTABLE;
 
 pub fn cache_file_path(cache_path: &str, block_no: u64, is_l1: bool) -> String {
     let prefix = if is_l1 { "l1" } else { "l2" };
-    format!("{}/{}-{}.json.gz", cache_path, prefix, block_no)
+    format!("{}/{}.{}.json.gz", cache_path, block_no, prefix)
 }
 
 pub fn guest_executable_path(guest_path: &str, proof_type: &str) -> String {
     format!("{}/{}/{}", guest_path, proof_type, RAIKO_GUEST_EXECUTABLE)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_file_prefix() {
+        let path = std::path::Path::new("/tmp/ethereum/1234.l1.json.gz");
+        let prefix = path.file_prefix().unwrap();
+        assert_eq!(prefix, "1234");
+    }
 }
