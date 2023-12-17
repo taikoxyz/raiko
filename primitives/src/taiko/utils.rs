28721cead1d81496497a9c6030db8fcb55b6a473
john xu <john@taiko.xyz>
2023-12-17 14:25:48 +0000

    Add test for string_to_bytes32 function

--- primitives/src/taiko/utils.rs
+++ primitives/src/taiko/utils.rs
@@ -6,3 +6,20 @@ pub fn string_to_bytes32(input: &[u8]) -> [u8; 32] {
     bytes[..len].copy_from_slice(&input[..len]);
     bytes
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn string_to_bytes32_test() {
+        let input = "";
+        let byte = string_to_bytes32(input.as_bytes());
+        assert_eq!(
+            byte,
+            [
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0
+            ]
+        );
+    }
+}
