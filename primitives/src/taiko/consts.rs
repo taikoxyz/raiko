38aa0902e5b93ec5e7a0c5af22085cd6e59293da
john xu <john@taiko.xyz>
2023-12-28 11:18:31 +0000

    Update taiko consts.rs: modify anchor gas limit and add new addresses

--- primitives/src/taiko/consts.rs
+++ primitives/src/taiko/consts.rs
@@ -3,6 +3,10 @@ use std::str::FromStr;
 use alloy_primitives::{uint, Address, U256};
 use once_cell::sync::Lazy;
 
+pub const ANCHOR_GAS_LIMIT: u64 = 250_000;
+pub static BLOCK_GAS_LIMIT: Lazy<U256> = Lazy::new(|| uint!(15250000_U256));
+
+// need modify blew four address when redeploy contract
 pub static L1_SIGNAL_SERVICE: Lazy<Address> = Lazy::new(|| {
     Address::from_str("0xb36a8742CDeA0195f5e659DED165FeA0dCC9F485")
         .expect("invalid l1 signal service")
@@ -12,9 +16,6 @@ pub static L2_SIGNAL_SERVICE: Lazy<Address> = Lazy::new(|| {
     Address::from_str("0x1670080000000000000000000000000000000005")
         .expect("invalid l2 signal service")
 });
-
-pub const ANCHOR_GAS_LIMIT: u64 = 250_000;
-pub static BLOCK_GAS_LIMIT: Lazy<U256> = Lazy::new(|| uint!(15250000_U256));
 
 pub static GOLDEN_TOUCH_ACCOUNT: Lazy<Address> = Lazy::new(|| {
     Address::from_str("0x0000777735367b36bC9B61C50022d9D0700dB4Ec")
