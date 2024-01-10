4d5b28bbd7130339354e83ee14ad356b5f0ee28d
john xu <john@taiko.xyz>
2024-01-11 00:06:32 +0800

    Remove unused import in rpc_provider.rs

--- lib/src/host/provider/rpc_provider.rs
+++ lib/src/host/provider/rpc_provider.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(feature = "taiko")]
-use anyhow::Context;
 use anyhow::{anyhow, Result};
 #[cfg(feature = "taiko")]
 use ethers_core::types::Filter;
