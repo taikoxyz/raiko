63862a5f569bc208363bb90b68b952f2b543de2f
john xu <john@taiko.xyz>
2024-01-11 15:54:26 +0800

    fix: remove cache logic

--- raiko-host/src/prover/execution.rs
+++ raiko-host/src/prover/execution.rs
@@ -4,34 +4,19 @@ use super::{
     context::Context,
     error::Result,
     prepare_input::prepare_input,
-    proof::{
-        cache::{Cache, CacheKey},
-        sgx::execute_sgx,
-        ProofType,
-    },
-    request::{ProofRequest, ProofResponse, SgxResponse},
+    proof::{cache::Cache, sgx::execute_sgx},
+    request::{ProofRequest, ProofResponse},
 };
 // use crate::rolling::prune_old_caches;
 
-pub async fn execute(cache: &Cache, ctx: &Context, req: &ProofRequest) -> Result<ProofResponse> {
+pub async fn execute(_cache: &Cache, ctx: &Context, req: &ProofRequest) -> Result<ProofResponse> {
     // 1. load input data into cache path
     let _ = prepare_input::<TaikoStrategyBundle>(ctx, req).await?;
     // 2. run proof
     // prune_old_caches(&ctx.cache_path, ctx.max_caches);
     match req {
         ProofRequest::Sgx(req) => {
-            let cache_key = CacheKey {
-                proof_type: ProofType::Sgx,
-                block: req.block,
-                prover: req.prover,
-                graffiti: req.graffiti,
-            };
-            let cached = cache.get(&cache_key);
-            if let Some(proof) = cached {
-                return Ok(ProofResponse::Sgx(SgxResponse { proof }));
-            }
             let resp = execute_sgx(ctx, req).await?;
-            cache.set(cache_key, resp.proof.clone());
             Ok(ProofResponse::Sgx(resp))
         }
         ProofRequest::PseZk(_) => todo!(),
