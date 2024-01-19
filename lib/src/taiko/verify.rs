5b9a08ac9a24e2e366a303f8bd66c25236297a8a
john xu <john@taiko.xyz>
2024-01-19 10:02:29 +0800

    Add transactions field to Header struct

--- lib/src/taiko/verify.rs
+++ lib/src/taiko/verify.rs
@@ -1,6 +1,9 @@
 use alloy_sol_types::SolValue;
 use anyhow::{anyhow, Result};
-use zeth_primitives::{block::Header, ethers::from_ethers_h256, taiko::ProtocolInstance};
+use zeth_primitives::{
+    block::Header, ethers::from_ethers_h256, taiko::ProtocolInstance,
+    transactions::EthereumTransaction,
+};
 
 use crate::taiko::host::TaikoExtra;
 
@@ -15,10 +18,16 @@ pub fn verify(header: &Header, pi: &ProtocolInstance, extra: &TaikoExtra) -> Res
     }
     // check the block hash
     if Some(header.hash()) != extra.l2_fini_block.hash.map(from_ethers_h256) {
+        let txs: Vec<EthereumTransaction> = extra
+            .l2_fini_block
+            .transactions
+            .iter()
+            .filter_map(|tx| tx.clone().try_into().ok())
+            .collect();
         return Err(anyhow!(
             "block hash mismatch, expected: {:?}, got: {:?}",
-            extra.l2_fini_block,
-            header,
+            txs,
+            header.transactions,
         ));
     }
 
