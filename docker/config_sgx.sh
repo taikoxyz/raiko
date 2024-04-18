487a92df497440ae030fc95b3bbe40ff5dc14197
smtmfft <smtm@taiko.xyz>
2024-04-18 11:18:05 +0800

    add --init back for offline bootstrap, to avoid config-id-after-prove issue.

--- docker/config_sgx.sh
+++ docker/config_sgx.sh
@@ -15,7 +15,7 @@ echo "Ready to config container: $CONTAINER_ID"
 echo "Old config"
 docker exec $CONTAINER_ID cat /etc/raiko/config.sgx.json
 echo
-docker exec $CONTAINER_ID sed -i "s/456/$NEW_ID/" /etc/raiko/config.sgx.json
+docker exec $CONTAINER_ID sed -i "s/123456/$NEW_ID/" /etc/raiko/config.sgx.json
 # post-check update
 echo "New config"
 docker exec $CONTAINER_ID cat /etc/raiko/config.sgx.json
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,14 +7,14 @@ RAIKO_DOCKER_VOLUME_PATH="/root/.config/raiko"
 RAIKO_DOCKER_VOLUME_CONFIG_PATH="$RAIKO_DOCKER_VOLUME_PATH/config"
 RAIKO_DOCKER_VOLUME_SECRETS_PATH="$RAIKO_DOCKER_VOLUME_PATH/secrets"
 RAIKO_DOCKER_VOLUME_PRIV_KEY_PATH="$RAIKO_DOCKER_VOLUME_SECRETS_PATH/priv.key"
-RAIKO_GUEST_APP_DIR="/opt/raiko/provers/sgx"
+RAIKO_APP_DIR="/opt/raiko/bin"
 RAIKO_GUEST_APP_FILENAME="sgx-guest"
-RAIKO_INPUT_MANIFEST_FILENAME="$RAIKO_GUEST_APP_FILENAME.manifest"
+RAIKO_INPUT_MANIFEST_FILENAME="$RAIKO_GUEST_APP_FILENAME.docker.manifest.template"
 RAIKO_OUTPUT_MANIFEST_FILENAME="$RAIKO_GUEST_APP_FILENAME.manifest.sgx"
 RAIKO_SIGNED_MANIFEST_FILENAME="$RAIKO_GUEST_APP_FILENAME.sig"
 
 function sign_gramine_manifest() {
-    cd "$RAIKO_GUEST_APP_DIR"
+    cd "$RAIKO_APP_DIR"
     gramine-sgx-sign --manifest "$RAIKO_INPUT_MANIFEST_FILENAME" --output "$RAIKO_OUTPUT_MANIFEST_FILENAME"
     mkdir -p "$RAIKO_DOCKER_VOLUME_CONFIG_PATH"
     cp "$RAIKO_OUTPUT_MANIFEST_FILENAME" "$RAIKO_SIGNED_MANIFEST_FILENAME" "$RAIKO_DOCKER_VOLUME_CONFIG_PATH"
@@ -23,7 +23,7 @@ function sign_gramine_manifest() {
 
 function bootstrap() {
     mkdir -p "$RAIKO_DOCKER_VOLUME_SECRETS_PATH"
-    cd "$RAIKO_GUEST_APP_DIR"
+    cd "$RAIKO_APP_DIR"
     gramine-sgx "$RAIKO_GUEST_APP_FILENAME" bootstrap
     cd -
 }
@@ -35,24 +35,20 @@ else
 fi
 
 sed -i "s/https:\/\/localhost:8081/https:\/\/${MY_PCCS_HOST}/g" /etc/sgx_default_qcnl.conf
-
+sed -i "s/123456/${SGX_INSTANCE_ID}/" /etc/raiko/config.sgx.json
 /restart_aesm.sh
 
-/opt/raiko/bin/raiko-host "$@"
+echo $#
+if [[ $# -eq 1 && $1 == "--init" ]]; then
+    echo "start bootstrap"
+    bootstrap
+else
+    echo "start proving"
+    if [[ ! -f "$RAIKO_DOCKER_VOLUME_PRIV_KEY_PATH" ]]; then
+        echo "Application was not bootstrapped. "\
+             "$RAIKO_DOCKER_VOLUME_PRIV_KEY_PATH is missing. Bootstrap it first." >&2
+        exit 1
+    fi
 
-# if [[ $# -eq 1 && $1 == "--init" ]]; then
-#     if [[ ! -f "$GRAMINE_PRIV_KEY" ]]; then
-#         gramine-sgx-gen-private-key
-#     fi
-#     sign_gramine_manifest
-#     bootstrap
-# else
-#     if [[ ! -f "$RAIKO_DOCKER_VOLUME_PRIV_KEY_PATH" ]]; then
-#         echo "Application was not bootstrapped. "\
-#              "$RAIKO_DOCKER_VOLUME_PRIV_KEY_PATH is missing. Bootstrap it first." >&2
-#         exit 1
-#     fi
-
-#     sign_gramine_manifest
-#     /opt/raiko/bin/raiko-host "$@"
-# fi
+    /opt/raiko/bin/raiko-host "$@"
+fi
