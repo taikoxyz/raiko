13ca16a920a091bb38eba348e235b86baf459d0e
smtmfft <smtm@taiko.xyz>
2024-05-02 02:11:12 +0000

    feat(raiko): enable kzg blob check

--- provers/sp1/guest/src/main.rs
+++ provers/sp1/guest/src/main.rs
@@ -8,6 +8,35 @@ use raiko_lib::{
     input::{GuestInput, GuestOutput, WrappedHeader},
 };
 
+use std::{
+    alloc::{alloc, handle_alloc_error, Layout},
+    ffi::c_void,
+};
+
+#[no_mangle]
+// TODO ideally this is c_size_t, but not stabilized (not guaranteed to be usize on all archs)
+unsafe extern "C" fn malloc(size: usize) -> *mut c_void {
+    let layout = Layout::from_size_align(size, 4).expect("unable to allocate more memory");
+    let ptr = alloc(layout);
+
+    if ptr.is_null() {
+        handle_alloc_error(layout);
+    }
+
+    ptr as *mut c_void
+}
+
+#[no_mangle]
+// TODO shouldn't need to zero allocated bytes since the zkvm memory is zeroed, might want to zero anyway
+unsafe extern "C" fn calloc(size: usize) -> *mut c_void {
+    malloc(size)
+}
+
+#[no_mangle]
+unsafe extern "C" fn free(_size: *const c_void) {
+    // Intentionally a no-op, since the zkvm allocator is a bump allocator
+}
+
 pub fn main() {
     let input = sp1_zkvm::io::read::<GuestInput>();
     let build_result = TaikoStrategy::build_from(&input);
