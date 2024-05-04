90647725f50a340049a9c84ba743e1c4f846b3e4
Ceciliaz030 <zhangyixin319@gmail.com>
2024-05-04 19:03:52 +0000

    trait Pipeline impl in specific builder

--- pipeline/src/lib.rs
+++ pipeline/src/lib.rs
@@ -3,91 +3,18 @@ mod executor;
 #[cfg(feature = "risc0")]
 mod risc0_util;
 
-use builder::{parse_metadata, CommandBuilder, GuestMetadata};
-use cargo_metadata::Metadata;
+pub use builder::{parse_metadata, CommandBuilder, GuestMetadata};
+pub use cargo_metadata::Metadata;
 use once_cell::sync::OnceCell;
 use std::path::PathBuf;
 
-static ROOT_DIR: OnceCell<PathBuf> = OnceCell::new();
+pub static ROOT_DIR: OnceCell<PathBuf> = OnceCell::new();
 
 pub trait Pipeline {
     fn new(root: &str, profile: &str) -> Self;
     fn builder(&self) -> CommandBuilder;
     fn bins(&self, bins: &[&str], dest: &str);
     fn tests(&self, bins: &[&str], dest: &str);
-}
-#[cfg(feature = "sp1")]
-pub use sp1::*;
-
-#[cfg(feature = "sp1")]
-pub mod sp1 {
-
-    use super::*;
-
-    pub struct Sp1Pipeline {
-        pub meta: Metadata,
-        pub profile: String,
-    }
-
-    impl Pipeline for Sp1Pipeline {
-        fn new(root: &str, profile: &str) -> Self {
-            ROOT_DIR.get_or_init(|| PathBuf::from(root));
-            Sp1Pipeline {
-                meta: parse_metadata(root),
-                profile: profile.to_string(),
-            }
-        }
-
-        fn builder(&self) -> CommandBuilder {
-            CommandBuilder::new(&self.meta, "riscv32im-succinct-zkvm-elf", "succinct")
-                .rust_flags(&[
-                    "passes=loweratomic",
-                    "link-arg=-Ttext=0x00200800",
-                    "panic=abort",
-                ])
-                .cc_compiler("gcc".into())
-                .c_flags(&["/opt/riscv/bin/riscv32-unknown-elf-gcc"])
-                .custom_args(&["--ignore-rust-version"])
-        }
-
-        fn bins(&self, names: &[&str], dest: &str) {
-            rerun_if_changed(&[]);
-            let bins = self.meta.get_bins(names);
-            let builder = self.builder();
-            let executor = builder.build_command(&self.profile, &bins);
-            println!(
-                "executor: \n   ${:?}\ntargets: \n   {:?}",
-                executor.cmd, executor.artifacts
-            );
-            if executor.artifacts.is_empty() {
-                panic!("No artifacts to build");
-            }
-            executor
-                .execute()
-                .expect("Execution failed")
-                .sp1_placement(dest)
-                .expect("Failed to export Sp1 artifacts");
-        }
-
-        fn tests(&self, names: &[&str], dest: &str) {
-            rerun_if_changed(&[]);
-            let tests = self.meta.get_tests(names);
-            let builder = self.builder();
-            let executor = builder.test_command(&self.profile, &tests);
-            println!(
-                "executor: \n   ${:?}\ntargets: \n   {:?}",
-                executor.cmd, executor.artifacts
-            );
-            if executor.artifacts.is_empty() {
-                panic!("No artifacts to build");
-            }
-            executor
-                .execute()
-                .expect("Execution failed")
-                .sp1_placement(dest)
-                .expect("Failed to export Sp1 artifacts");
-        }
-    }
 }
 
 #[cfg(feature = "risc0")]
