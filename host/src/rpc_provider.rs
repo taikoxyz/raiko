595c76218cf1ed939c569bd388a4523767c88333
Petar Vujovic <petarvujovic98@gmail.com>
2024-05-07 15:30:50 +0200

    refactor: Use error propagation instead of panicking

--- host/src/rpc_provider.rs
+++ host/src/rpc_provider.rs
@@ -3,13 +3,16 @@ use alloy_provider::{ProviderBuilder, ReqwestProvider, RootProvider};
 use alloy_rpc_client::{ClientBuilder, RpcClient};
 use alloy_rpc_types::{Block, BlockId, BlockNumberOrTag, EIP1186AccountProofResponse};
 use alloy_transport_http::Http;
-use anyhow::Result;
 use raiko_lib::{clear_line, inplace_print};
 use reqwest_alloy::Client;
 use revm::primitives::{AccountInfo, Bytecode};
 use std::collections::HashMap;
 
-use crate::{raiko::BlockDataProvider, MerkleProof};
+use crate::{
+    error::{HostError, HostResult},
+    raiko::BlockDataProvider,
+    MerkleProof,
+};
 
 pub struct RpcBlockDataProvider {
     pub provider: ReqwestProvider,
@@ -18,13 +21,14 @@ pub struct RpcBlockDataProvider {
 }
 
 impl RpcBlockDataProvider {
-    pub fn new(url: &str, block_number: u64) -> Self {
-        let url = reqwest::Url::parse(url).expect("invalid rpc url");
-        Self {
+    pub fn new(url: &str, block_number: u64) -> HostResult<Self> {
+        let url =
+            reqwest::Url::parse(&url).map_err(|_| HostError::RPC("Invalid RPC URL".to_owned()))?;
+        Ok(Self {
             provider: ProviderBuilder::new().on_provider(RootProvider::new_http(url.clone())),
             client: ClientBuilder::default().http(url),
             block_number,
-        }
+        })
     }
 
     pub fn provider(&self) -> &ReqwestProvider {
@@ -33,30 +37,42 @@ impl RpcBlockDataProvider {
 }
 
 impl BlockDataProvider for RpcBlockDataProvider {
-    async fn get_blocks(
-        &self,
-        blocks_to_fetch: &[(u64, bool)],
-    ) -> Result<Vec<Block>, anyhow::Error> {
-        let mut all_blocks = Vec::new();
+    async fn get_blocks(&self, blocks_to_fetch: &[(u64, bool)]) -> HostResult<Vec<Block>> {
+        let mut all_blocks = Vec::with_capacity(blocks_to_fetch.len());
 
         let max_batch_size = 32;
         for blocks_to_fetch in blocks_to_fetch.chunks(max_batch_size) {
             let mut batch = self.client.new_batch();
-            let mut requests = vec![];
+            let mut requests = Vec::with_capacity(max_batch_size);
 
             for (block_number, full) in blocks_to_fetch.iter() {
-                requests.push(Box::pin(batch.add_call(
-                    "eth_getBlockByNumber",
-                    &(BlockNumberOrTag::from(*block_number), full),
-                )?));
+                requests.push(Box::pin(
+                    batch
+                        .add_call(
+                            "eth_getBlockByNumber",
+                            &(BlockNumberOrTag::from(*block_number), full),
+                        )
+                        .map_err(|_| {
+                            HostError::RPC(
+                                "Failed adding eth_getBlockByNumber call to batch".to_owned(),
+                            )
+                        })?,
+                ));
             }
 
-            batch.send().await?;
+            batch
+                .send()
+                .await
+                .map_err(|_| HostError::RPC("Error sending batch request".to_owned()))?;
 
-            let mut blocks = vec![];
+            let mut blocks = Vec::with_capacity(max_batch_size);
             // Collect the data from the batch
             for request in requests.into_iter() {
-                blocks.push(request.await?);
+                blocks.push(
+                    request
+                        .await
+                        .map_err(|_| HostError::RPC("Error collecting request data".to_owned()))?,
+                );
             }
 
             all_blocks.append(&mut blocks);
@@ -65,16 +81,16 @@ impl BlockDataProvider for RpcBlockDataProvider {
         Ok(all_blocks)
     }
 
-    async fn get_accounts(&self, accounts: &[Address]) -> Result<Vec<AccountInfo>, anyhow::Error> {
-        let mut all_accounts = Vec::new();
+    async fn get_accounts(&self, accounts: &[Address]) -> HostResult<Vec<AccountInfo>> {
+        let mut all_accounts = Vec::with_capacity(accounts.len());
 
         let max_batch_size = 250;
         for accounts in accounts.chunks(max_batch_size) {
             let mut batch = self.client.new_batch();
 
-            let mut nonce_requests = Vec::new();
-            let mut balance_requests = Vec::new();
-            let mut code_requests = Vec::new();
+            let mut nonce_requests = Vec::with_capacity(max_batch_size);
+            let mut balance_requests = Vec::with_capacity(max_batch_size);
+            let mut code_requests = Vec::with_capacity(max_batch_size);
 
             for address in accounts {
                 nonce_requests.push(Box::pin(
@@ -83,7 +99,11 @@ impl BlockDataProvider for RpcBlockDataProvider {
                             "eth_getTransactionCount",
                             &(address, Some(BlockId::from(self.block_number))),
                         )
-                        .unwrap(),
+                        .map_err(|_| {
+                            HostError::RPC(
+                                "Failed adding eth_getTransactionCount call to batch".to_owned(),
+                            )
+                        })?,
                 ));
                 balance_requests.push(Box::pin(
                     batch
@@ -91,7 +111,9 @@ impl BlockDataProvider for RpcBlockDataProvider {
                             "eth_getBalance",
                             &(address, Some(BlockId::from(self.block_number))),
                         )
-                        .unwrap(),
+                        .map_err(|_| {
+                            HostError::RPC("Failed adding eth_getBalance call to batch".to_owned())
+                        })?,
                 ));
                 code_requests.push(Box::pin(
                     batch
@@ -99,30 +121,43 @@ impl BlockDataProvider for RpcBlockDataProvider {
                             "eth_getCode",
                             &(address, Some(BlockId::from(self.block_number))),
                         )
-                        .unwrap(),
+                        .map_err(|_| {
+                            HostError::RPC("Failed adding eth_getCode call to batch".to_owned())
+                        })?,
                 ));
             }
 
-            batch.send().await?;
+            batch
+                .send()
+                .await
+                .map_err(|_| HostError::RPC("Error sending batch request".to_owned()))?;
 
             let mut accounts = vec![];
             // Collect the data from the batch
-            for (nonce_request, (balance_request, code_request)) in nonce_requests
+            for ((nonce_request, balance_request), code_request) in nonce_requests
                 .into_iter()
-                .zip(balance_requests.into_iter().zip(code_requests.into_iter()))
+                .zip(balance_requests.into_iter())
+                .zip(code_requests.into_iter())
             {
                 let (nonce, balance, code) = (
-                    nonce_request.await?,
-                    balance_request.await?,
-                    code_request.await?,
+                    nonce_request.await.map_err(|_| {
+                        HostError::RPC("Failed to collect nonce request".to_owned())
+                    })?,
+                    balance_request.await.map_err(|_| {
+                        HostError::RPC("Failed to collect balance request".to_owned())
+                    })?,
+                    code_request
+                        .await
+                        .map_err(|_| HostError::RPC("Failed to collect code request".to_owned()))?,
                 );
 
-                let account_info = AccountInfo::new(
-                    balance,
-                    nonce.try_into().unwrap(),
-                    Bytecode::new_raw(code.clone()).hash_slow(),
-                    Bytecode::new_raw(code),
-                );
+                let nonce = nonce.try_into().map_err(|_| {
+                    HostError::Conversion("Failed to convert nonce to u64".to_owned())
+                })?;
+
+                let bytecode = Bytecode::new_raw(code);
+
+                let account_info = AccountInfo::new(balance, nonce, bytecode.hash_slow(), bytecode);
 
                 accounts.push(account_info);
             }
@@ -133,17 +168,14 @@ impl BlockDataProvider for RpcBlockDataProvider {
         Ok(all_accounts)
     }
 
-    async fn get_storage_values(
-        &self,
-        accounts: &[(Address, U256)],
-    ) -> Result<Vec<U256>, anyhow::Error> {
-        let mut all_values = Vec::new();
+    async fn get_storage_values(&self, accounts: &[(Address, U256)]) -> HostResult<Vec<U256>> {
+        let mut all_values = Vec::with_capacity(accounts.len());
 
         let max_batch_size = 1000;
         for accounts in accounts.chunks(max_batch_size) {
             let mut batch = self.client.new_batch();
 
-            let mut requests = Vec::new();
+            let mut requests = Vec::with_capacity(max_batch_size);
 
             for (address, key) in accounts {
                 requests.push(Box::pin(
@@ -152,16 +184,27 @@ impl BlockDataProvider for RpcBlockDataProvider {
                             "eth_getStorageAt",
                             &(address, key, Some(BlockId::from(self.block_number))),
                         )
-                        .unwrap(),
+                        .map_err(|_| {
+                            HostError::RPC(
+                                "Failed adding eth_getStorageAt call to batch".to_owned(),
+                            )
+                        })?,
                 ));
             }
 
-            batch.send().await?;
+            batch
+                .send()
+                .await
+                .map_err(|_| HostError::RPC("Error sending batch request".to_owned()))?;
 
-            let mut values = vec![];
+            let mut values = Vec::with_capacity(max_batch_size);
             // Collect the data from the batch
             for request in requests.into_iter() {
-                values.push(request.await?);
+                values.push(
+                    request
+                        .await
+                        .map_err(|_| HostError::RPC("Error collecting request data".to_owned()))?,
+                );
             }
 
             all_values.append(&mut values);
@@ -176,7 +219,7 @@ impl BlockDataProvider for RpcBlockDataProvider {
         accounts: HashMap<Address, Vec<U256>>,
         offset: usize,
         num_storage_proofs: usize,
-    ) -> Result<MerkleProof, anyhow::Error> {
+    ) -> HostResult<MerkleProof> {
         let mut storage_proofs: MerkleProof = HashMap::new();
         let mut idx = offset;
 
@@ -197,6 +240,7 @@ impl BlockDataProvider for RpcBlockDataProvider {
             let mut batch_size = 0;
             while !accounts.is_empty() && batch_size < batch_limit {
                 let mut address_to_remove = None;
+
                 if let Some((address, keys)) = accounts.iter_mut().next() {
                     // Calculate how many keys we can still process
                     let num_keys_to_process = if batch_size + keys.len() < batch_limit {
@@ -227,7 +271,11 @@ impl BlockDataProvider for RpcBlockDataProvider {
                                     BlockId::from(block_number),
                                 ),
                             )
-                            .unwrap(),
+                            .map_err(|_| {
+                                HostError::RPC(
+                                    "Failed adding eth_getProof call to batch".to_owned(),
+                                )
+                            })?,
                     ));
 
                     // Keep track of how many keys were processed
@@ -242,11 +290,16 @@ impl BlockDataProvider for RpcBlockDataProvider {
             }
 
             // Send the batch
-            batch.send().await?;
+            batch
+                .send()
+                .await
+                .map_err(|_| HostError::RPC("Error sending batch request".to_owned()))?;
 
             // Collect the data from the batch
             for request in requests.into_iter() {
-                let mut proof = request.await?;
+                let mut proof = request
+                    .await
+                    .map_err(|_| HostError::RPC("Error collecting request data".to_owned()))?;
                 idx += proof.storage_proof.len();
                 if let Some(map_proof) = storage_proofs.get_mut(&proof.address) {
                     map_proof.storage_proof.append(&mut proof.storage_proof);
