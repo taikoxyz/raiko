2513aecacc9c85c1e86c184d2a53c9da0e61fbf3
Petar Vujovic <petarvujovic98@gmail.com>
2024-05-07 14:47:10 +0200

    feat: Add new error variants

--- host/src/error.rs
+++ host/src/error.rs
@@ -24,6 +24,18 @@ pub enum HostError {
     #[schema(value_type = Value)]
     Io(#[from] std::io::Error),
 
+    /// For preflight errors.
+    #[error("There was a error running the preflight: {0}")]
+    Preflight(String),
+
+    /// For invalid type conversion.
+    #[error("Invalid conversion: {0}")]
+    Conversion(String),
+
+    /// For RPC errors.
+    #[error("There was a error with the RPC provider: {0}")]
+    RPC(String),
+
     /// For Serde errors.
     #[error("There was a deserialization error: {0}")]
     #[schema(value_type = Value)]
@@ -37,7 +49,12 @@ pub enum HostError {
     /// For errors produced by the guest provers.
     #[error("There was a error with a guest prover: {0}")]
     #[schema(value_type = Value)]
-    GuestError(#[from] ProverError),
+    Guest(#[from] ProverError),
+
+    /// For db errors.
+    #[error("There was a error with the db: {0}")]
+    #[schema(value_type = Value)]
+    Db(raiko_lib::mem_db::DbError),
 
     /// For requesting a proof of a type that is not supported.
     #[error("Feature not supported: {0}")]
@@ -50,28 +67,28 @@ pub enum HostError {
     Anyhow(#[from] anyhow::Error),
 }
 
+impl From<raiko_lib::mem_db::DbError> for HostError {
+    fn from(e: raiko_lib::mem_db::DbError) -> Self {
+        HostError::Db(e)
+    }
+}
+
 impl IntoResponse for HostError {
     fn into_response(self) -> axum::response::Response {
+        use HostError::*;
         match self {
-            HostError::InvalidProofType(e)
-            | HostError::InvalidRequestConfig(e)
-            | HostError::InvalidAddress(e) => {
+            InvalidProofType(e) | InvalidRequestConfig(e) | InvalidAddress(e) => {
                 (StatusCode::BAD_REQUEST, e.to_string()).into_response()
             }
-            HostError::Io(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
-            HostError::Serde(e) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response()
-            }
-            HostError::Anyhow(e) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response()
-            }
-            HostError::JoinHandle(e) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response()
-            }
-            HostError::GuestError(e) => {
-                (StatusCode::FAILED_DEPENDENCY, e.to_string()).into_response()
-            }
-            HostError::FeatureNotSupportedError(e) => {
+            Conversion(e) | Preflight(e) => (StatusCode::INTERNAL_SERVER_ERROR, e).into_response(),
+            Io(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+            Serde(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+            Anyhow(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+            JoinHandle(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+            Guest(e) => (StatusCode::FAILED_DEPENDENCY, e.to_string()).into_response(),
+            RPC(e) => (StatusCode::FAILED_DEPENDENCY, e.to_string()).into_response(),
+            Db(e) => (StatusCode::FAILED_DEPENDENCY, e.to_string()).into_response(),
+            FeatureNotSupportedError(e) => {
                 (StatusCode::METHOD_NOT_ALLOWED, e.to_string()).into_response()
             }
         }
