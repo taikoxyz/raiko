238982fd6ddebd7ed1e362918227dd2da1156ce5
Petar Vujovic <petarvujovic98@gmail.com>
2024-05-08 13:31:52 +0200

    refactor: Make doc creation a oneliner

--- host/src/docs.rs
+++ host/src/docs.rs
@@ -2,8 +2,5 @@ use raiko_host::server::api::create_docs;
 use utoipa_scalar::Scalar;
 
 fn main() {
-    let docs = create_docs();
-    let scalar = Scalar::new(docs);
-    let html = scalar.to_html();
-    println!("{html}");
+    println!("{}", Scalar::new(create_docs()).to_html());
 }
