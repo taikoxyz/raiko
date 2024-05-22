ac3f27cba7224999b14ad4fbb5953521b526f4a4
johntaiko <john@taiko.xyz>
2024-05-22 17:53:47 +0800

    fix: some docker stuff (#239)

--- script/publish-image.sh
+++ script/publish-image.sh
@@ -30,7 +30,7 @@ for feature in "${features[@]}"; do
 	fi
 
 	echo "Build and push $1:$tag..."
-	docker buildx build --no-cache ./ \
+	docker buildx build ./ \
 		--platform linux/amd64 \
 		-t raiko:$tag \
 		$build_flags \
