892326ab55459f2257bcc01aee8563cc6cd89aec
Ceciliaz030 <zhangyixin319@gmail.com>
2024-05-22 20:24:10 +0000

    fmt

--- provers/risc0/driver/src/methods/risc0_guest.rs
+++ provers/risc0/driver/src/methods/risc0_guest.rs
@@ -1,4 +1,7 @@
-
-pub const RISC0_GUEST_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
-pub const RISC0_GUEST_ID: [u32; 8] = [405814261, 645472475, 3368860906, 1069727513, 2312368391, 2313520942, 2156489466, 779875178];
-pub const RISC0_GUEST_PATH: &str = r#"/home/ubuntu/raiko/provers/risc0/guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest"#;
+pub const RISC0_GUEST_ELF: &[u8] =
+    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
+pub const RISC0_GUEST_ID: [u32; 8] = [
+    405814261, 645472475, 3368860906, 1069727513, 2312368391, 2313520942, 2156489466, 779875178,
+];
+pub const RISC0_GUEST_PATH: &str =
+    r#"/home/ubuntu/raiko/provers/risc0/guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest"#;
