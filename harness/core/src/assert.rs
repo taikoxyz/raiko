fb3862e3dcf26923ed38ad03b98a069cbfe55743
Petar Vujovic <petarvujovic98@gmail.com>
2024-05-25 16:29:56 +0700

    refactor(harness): small code readability and lint fixes

--- harness/core/src/assert.rs
+++ harness/core/src/assert.rs
@@ -1,15 +1,17 @@
-use std::{fmt::Debug, sync::Mutex};
+use std::fmt::Debug;
 
 pub trait DynAssertion: Send + Sync {
     fn display(&self);
     fn failed(&self) -> bool;
 }
+
 #[derive(Debug)]
 pub struct AssertEQ<T> {
     pub left: T,
     pub right: T,
     pub result: bool,
 }
+
 #[derive(Debug)]
 pub struct Assert {
     pub result: bool,
@@ -32,6 +34,7 @@ impl<T: Debug + Clone + Send + Sync> DynAssertion for Assertion<T> {
         }
     }
 }
+
 pub struct AssertionLog {
     pub assertions: Vec<Box<dyn DynAssertion>>,
 }
@@ -59,36 +62,38 @@ impl AssertionLog {
 
     pub fn display_failures(&self, start: usize, end: usize) {
         for i in start..end {
-            if self.assertions[i].failed() {
-                self.assertions[i].display();
+            if let Some(assertion) = self.assertions.get(i) {
+                if assertion.failed() {
+                    assertion.display();
+                }
             }
         }
     }
 
     pub fn summarize(&self, start: usize, end: usize) -> (usize, usize) {
-        let mut passed = 0;
-        let mut failed = 0;
-        for i in start..end {
-            if self.assertions[i].failed() {
-                failed += 1;
-            } else {
-                passed += 1;
+        (start..end).fold((0, 0), |(passed, failed), index| {
+            if let Some(assertion) = self.assertions.get(index) {
+                if assertion.failed() {
+                    (passed, failed + 1)
+                } else {
+                    (passed + 1, failed)
+                }
             }
-        }
-        (passed, failed)
+            (passed, failed)
+        })
     }
 }
 
 pub fn eval_assert(cond: bool, file: &str, line: u32) -> bool {
     if !cond {
-        println!("Assertion failed at {}:{}", file, line);
+        println!("Assertion failed at {file}:{line}");
     }
     cond
 }
 
 pub fn eval_assert_eq<T: PartialEq + std::fmt::Debug>(a: T, b: T, file: &str, line: u32) -> bool {
     if a != b {
-        println!("Assertion failed: {:?} != {:?} at {}:{}", a, b, file, line);
+        println!("Assertion failed: {a:?} != {b:?} at {file}:{line}");
         false
     } else {
         true
@@ -99,21 +104,18 @@ pub fn eval_assert_eq<T: PartialEq + std::fmt::Debug>(a: T, b: T, file: &str, li
 macro_rules! assert {
     ($cond:expr) => {
         let result = eval_assert(false, file!(), line!());
-        let log = $crate::ASSERTION_LOG.get_or_init(
-            || std::sync::Mutex::new(AssertionLog::new())
-        );
+        let log = $crate::ASSERTION_LOG.get_or_init(|| std::sync::Mutex::new(AssertionLog::new()));
         log.lock()
             .unwrap()
             .insert(Box::new(Assertion::<bool>::Cond(Assert { result })));
     };
 }
+
 #[macro_export]
 macro_rules! assert_eq {
     ($a:expr, $b:expr) => {
         let result = eval_assert_eq($a, $b, file!(), line!());
-        let log = $crate::ASSERTION_LOG.get_or_init(
-            || std::sync::Mutex::new(AssertionLog::new())
-        );
+        let log = $crate::ASSERTION_LOG.get_or_init(|| std::sync::Mutex::new(AssertionLog::new()));
         log.lock()
             .unwrap()
             .insert(Box::new(Assertion::<i32>::Eq(AssertEQ {
