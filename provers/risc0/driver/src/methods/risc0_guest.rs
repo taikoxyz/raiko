0ba89aecf846e2aeda41ff0c65410b9beb0d77f6
CeciliaZ030 <45245961+CeciliaZ030@users.noreply.github.com>
2024-06-09 01:23:10 -0400

    feat(prover): enable GuestInput serialization in native path (#281)
    * change native param & ProverError
    
    * change scirpt & compiled guest
    
    * fix prove-block.sh typo
    
    * fmt
    
    * fix test_proof_params
    
    * "write_guest_input_path": null
    
    * prover
    
    * fix
    
    * cargo update
    
    * api changes from AffinePoint::<Bn254, 16>::from
    
    * fix
    
    * remove default = ["enable"] & default = ["sgx"]
    
    * sgx-prover = { path = "../prover", features = ["enable"] }
    
    ---------
    
    Co-authored-by: d1onys1us <13951458+d1onys1us@users.noreply.github.com>

--- provers/risc0/driver/src/methods/risc0_guest.rs
+++ provers/risc0/driver/src/methods/risc0_guest.rs
@@ -1,7 +1,7 @@
 pub const RISC0_GUEST_ELF: &[u8] =
     include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
 pub const RISC0_GUEST_ID: [u32; 8] = [
-    405814261, 645472475, 3368860906, 1069727513, 2312368391, 2313520942, 2156489466, 779875178,
+    1444642754, 3434511061, 2910616417, 2829025913, 3284452016, 1678600137, 1001540409, 1336920303,
 ];
 pub const RISC0_GUEST_PATH: &str =
     r#"/home/ubuntu/raiko/provers/risc0/guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest"#;
