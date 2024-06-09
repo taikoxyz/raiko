4728d66bbb878a3e50c220554b0348eeb49f2295
CeciliaZ030 <zhangyixin319@gmail.com>
2024-06-09 06:13:30 +0000

    [dev-dependencies] substrate-bn = "0.6.0"

--- provers/risc0/driver/src/methods/risc0_guest.rs
+++ provers/risc0/driver/src/methods/risc0_guest.rs
@@ -1,7 +1,4 @@
-pub const RISC0_GUEST_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
-pub const RISC0_GUEST_ID: [u32; 8] = [
-    405814261, 645472475, 3368860906, 1069727513, 2312368391, 2313520942, 2156489466, 779875178,
-];
-pub const RISC0_GUEST_PATH: &str =
-    r#"/home/ubuntu/raiko/provers/risc0/guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest"#;
+
+pub const RISC0_GUEST_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
+pub const RISC0_GUEST_ID: [u32; 8] = [1072451331, 1684576653, 2445731149, 2744436035, 2568068848, 1574816461, 961397632, 2593052967];
+pub const RISC0_GUEST_PATH: &str = r#"/home/ubuntu/raiko/provers/risc0/guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest"#;
