89bb3e3a50c9804218fddba7da91c1148d650039
Brechtpd <Brechtp.devos@gmail.com>
2024-06-25 05:39:53 +0200

    fix local dependency

--- script/prove-block.sh
+++ script/prove-block.sh
@@ -32,8 +32,6 @@ elif [ "$chain" == "taiko_mainnet" ]; then
 	l1_network="ethereum"
 elif [ "$chain" == "taiko_a7" ]; then
 	l1_network="holesky"
-elif [ "$chain" == "taiko_mainnet" ]; then
-	l1_network="ethereum"
 else
 	echo "Using customized chain name $1. Please double check the RPCs."
 	l1_network="holesky"
