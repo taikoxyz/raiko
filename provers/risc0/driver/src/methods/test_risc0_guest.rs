5fbf93111d8f4889d898f5c8cc36eb48450f76d5
smtmfft <smtm@taiko.xyz>
2024-07-05 09:32:13 +0800

    remove useless constant
    Signed-off-by: smtmfft <smtm@taiko.xyz>

--- provers/risc0/driver/src/methods/test_risc0_guest.rs
+++ provers/risc0/driver/src/methods/test_risc0_guest.rs
@@ -1,7 +1,3 @@
-pub const TEST_RISC0_GUEST_ELF: &[u8] = include_bytes!(
-    "../../../guest/target/riscv32im-risc0-zkvm-elf/release/deps/risc0_guest-f2134d0a9dc1b778"
-);
-pub const TEST_RISC0_GUEST_ID: [u32; 8] = [
-    3875868406, 1668916610, 1404967724, 4014172081, 1679226880, 1967164957, 1092356078, 3079385105,
-];
-pub const TEST_RISC0_GUEST_PATH: &str = r#"provers/risc0/guest/target/riscv32im-risc0-zkvm-elf/release/deps/risc0_guest-f2134d0a9dc1b778"#;
+
+pub const TEST_RISC0_GUEST_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/deps/risc0_guest-9d0bc3658d2edeaa");
+pub const TEST_RISC0_GUEST_ID: [u32; 8] = [1938439720, 3200608207, 3901424709, 2031136574, 850058303, 3058786714, 2432413607, 2771178900];
