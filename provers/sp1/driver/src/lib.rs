c7673279c2440b56977af45435ffd4a632c20996
Petar Vujović <petarvujovic98@gmail.com>
2024-07-18 10:23:35 +0200

    feat(sp1): add cancellation support (#322)
    * feat(sp1): add cancellation support
    
    * fix(sp1): add prover code to proof key

--- provers/sp1/driver/src/lib.rs
+++ provers/sp1/driver/src/lib.rs
@@ -4,8 +4,12 @@ use raiko_lib::{
     prover::{IdStore, IdWrite, Proof, ProofKey, Prover, ProverConfig, ProverError, ProverResult},
 };
 use serde::{Deserialize, Serialize};
-use sp1_sdk::{ProverClient, SP1Stdin};
-use std::env;
+use sp1_sdk::{
+    network::client::NetworkClient,
+    proto::network::{ProofMode, ProofStatus, UnclaimReason},
+    ProverClient, SP1Stdin,
+};
+use std::{env, thread::sleep, time::Duration};
 use tracing::info as tracing_info;
 
 const ELF: &[u8] = include_bytes!("../../guest/elf/sp1-guest");
@@ -27,12 +31,14 @@ impl From<Sp1Response> for Proof {
 
 pub struct Sp1Prover;
 
+const SP1_PROVER_CODE: u8 = 1;
+
 impl Prover for Sp1Prover {
     async fn run(
         input: GuestInput,
-        _output: &GuestOutput,
+        output: &GuestOutput,
         _config: &ProverConfig,
-        _write: Option<&mut dyn IdWrite>,
+        writer: Option<&mut dyn IdWrite>,
     ) -> ProverResult<Proof> {
         // Write the input.
         let mut stdin = SP1Stdin::new();
@@ -41,9 +47,68 @@ impl Prover for Sp1Prover {
         // Generate the proof for the given program.
         let client = ProverClient::new();
         let (pk, vk) = client.setup(ELF);
-        let proof = client
-            .prove(&pk, stdin)
-            .map_err(|_| ProverError::GuestError("Sp1: proving failed".to_owned()))?;
+        let local = true;
+        let proof = match local {
+            true => {
+                let proof = client
+                    .prove(&pk, stdin)
+                    .map_err(|_| ProverError::GuestError("Sp1: proving failed".to_owned()))?;
+                Ok::<_, ProverError>(proof)
+            }
+            false => {
+                let private_key = env::var("SP1_PRIVATE_KEY").map_err(|_| {
+                    ProverError::GuestError(
+                        "SP1_PRIVATE_KEY must be set for remote proving".to_owned(),
+                    )
+                })?;
+                let network_client = NetworkClient::new(&private_key);
+                let proof_id = network_client
+                    .create_proof(&pk.elf, &stdin, ProofMode::Core, "v1.0.8-testnet")
+                    .await
+                    .map_err(|_| {
+                        ProverError::GuestError("Sp1: creating proof failed".to_owned())
+                    })?;
+                if let Some(writer) = writer {
+                    writer.store_id(
+                        (input.chain_spec.chain_id, output.hash, SP1_PROVER_CODE),
+                        proof_id.clone(),
+                    )?;
+                }
+                let proof = {
+                    let mut is_claimed = false;
+                    loop {
+                        let (status, maybe_proof) = network_client
+                            .get_proof_status(&proof_id)
+                            .await
+                            .map_err(|_| {
+                                ProverError::GuestError(
+                                    "Sp1: getting proof status failed".to_owned(),
+                                )
+                            })?;
+
+                        match status.status() {
+                            ProofStatus::ProofFulfilled => {
+                                break Ok(maybe_proof.unwrap());
+                            }
+                            ProofStatus::ProofClaimed => {
+                                if !is_claimed {
+                                    is_claimed = true;
+                                }
+                            }
+                            ProofStatus::ProofUnclaimed => {
+                                break Err(ProverError::GuestError(format!(
+                                    "Proof generation failed: {}",
+                                    status.unclaim_description()
+                                )));
+                            }
+                            _ => {}
+                        }
+                        sleep(Duration::from_secs(2));
+                    }
+                }?;
+                Ok::<_, ProverError>(proof)
+            }
+        }?;
 
         // Verify proof.
         client
@@ -70,7 +135,17 @@ impl Prover for Sp1Prover {
         .into())
     }
 
-    async fn cancel(_key: ProofKey, _store: Box<&mut dyn IdStore>) -> ProverResult<()> {
+    async fn cancel(key: ProofKey, store: Box<&mut dyn IdStore>) -> ProverResult<()> {
+        let proof_id = store.read_id(key)?;
+        let private_key = env::var("SP1_PRIVATE_KEY").map_err(|_| {
+            ProverError::GuestError("SP1_PRIVATE_KEY must be set for remote proving".to_owned())
+        })?;
+        let network_client = NetworkClient::new(&private_key);
+        network_client
+            .unclaim_proof(proof_id, UnclaimReason::Abandoned, "".to_owned())
+            .await
+            .map_err(|_| ProverError::GuestError("Sp1: couldn't unclaim proof".to_owned()))?;
+        store.remove_id(key)?;
         Ok(())
     }
 }
