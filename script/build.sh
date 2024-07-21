a6eadd43c3b3239efea81e43a670d5da468da400
CeciliaZ030 <zhangyixin319@gmail.com>
2024-07-21 15:50:22 +0000

    fix

--- script/build.sh
+++ script/build.sh
@@ -28,7 +28,7 @@ check_toolchain() {
 }
 
 if [ -z "${DEBUG}" ]; then
-	FLAGS=--release
+	PROFILE=--release
 else
 	echo "Warning: in debug mode"
 fi
@@ -49,18 +49,18 @@ if [ -z "$1" ] || [ "$1" == "native" ]; then
 	if [ -z "${RUN}" ]; then
 		if [ -z "${TEST}" ]; then
 			echo "Building native prover"
-			cargo build ${FLAGS}
+			cargo build ${PROFILE}
 		else
 			echo "Building native tests"
-			cargo test ${FLAGS} --no-run
+			cargo test ${PROFILE} --no-run
 		fi
 	else
 		if [ -z "${TEST}" ]; then
 			echo "Running native prover"
-			cargo run ${FLAGS}
+			cargo run ${PROFILE}
 		else
 			echo "Running native tests"
-			cargo test ${FLAGS}
+			cargo test ${PROFILE}
 		fi
 	fi
 fi
@@ -72,21 +72,21 @@ if [ -z "$1" ] || [ "$1" == "sgx" ]; then
 		export SGX_DIRECT=1
 		echo "SGX_DIRECT is set to $SGX_DIRECT"
 	fi
-	elif [ -z "${RUN}" ]; then
+	if [ -z "${RUN}" ]; then
 		if [ -z "${TEST}" ]; then
 			echo "Building SGX prover"
-			cargo ${TOOLCHAIN_SGX} build ${FLAGS} --features sgx
+			cargo ${TOOLCHAIN_SGX} build ${PROFILE} --features sgx
 		else
 			echo "Building SGX tests"
-			cargo ${TOOLCHAIN_SGX} test ${FLAGS} -p raiko-host -p sgx-prover --features "sgx enable" --no-run
+			cargo ${TOOLCHAIN_SGX} test ${PROFILE} -p raiko-host -p sgx-prover --features "sgx enable" --no-run
 		fi
 	else
 		if [ -z "${TEST}" ]; then
 			echo "Running SGX prover"
-			cargo ${TOOLCHAIN_SGX} run ${FLAGS} --features sgx
+			cargo ${TOOLCHAIN_SGX} run ${PROFILE} --features sgx
 		else
 			echo "Running SGX tests"
-			cargo ${TOOLCHAIN_SGX} test ${FLAGS} -p raiko-host -p sgx-prover --features "sgx enable"
+			cargo ${TOOLCHAIN_SGX} test ${PROFILE} -p raiko-host -p sgx-prover --features "sgx enable"
 		fi
 	fi
 fi
@@ -107,15 +107,15 @@ if [ -z "$1" ] || [ "$1" == "risc0" ]; then
 			echo "Building test elfs for Risc0 prover"
 			cargo ${TOOLCHAIN_RISC0} run --bin risc0-builder --features test,bench
 		fi
-		cargo ${TOOLCHAIN_RISC0} build ${FLAGS} --features risc0
+		cargo ${TOOLCHAIN_RISC0} build ${PROFILE} --features risc0
 	else
 		if [ -z "${TEST}" ]; then
 			echo "Running Risc0 prover"
-			cargo ${TOOLCHAIN_RISC0} run ${FLAGS} --features risc0
+			cargo ${TOOLCHAIN_RISC0} run ${PROFILE} --features risc0
 		else
 			echo "Running Risc0 tests"
-			cargo ${TOOLCHAIN_RISC0} test ${FLAGS} --lib risc0-driver --features risc0 -- run_unittest_elf
-			cargo ${TOOLCHAIN_RISC0} test ${FLAGS} -p raiko-host -p risc0-driver --features "risc0 enable"
+			cargo ${TOOLCHAIN_RISC0} test ${PROFILE} --lib risc0-driver --features risc0 -- run_unittest_elf
+			cargo ${TOOLCHAIN_RISC0} test ${PROFILE} -p raiko-host -p risc0-driver --features "risc0 enable"
 		fi
 	fi
 fi
@@ -135,15 +135,15 @@ if [ -z "$1" ] || [ "$1" == "sp1" ]; then
 			echo "Building test elfs for Sp1 prover"
 			cargo ${TOOLCHAIN_SP1} run --bin sp1-builder --features test,bench
 		fi
-		cargo ${TOOLCHAIN_SP1} build ${FLAGS} --features sp1
+		cargo ${TOOLCHAIN_SP1} build ${PROFILE} --features sp1
 	else
 		if [ -z "${TEST}" ]; then
 			echo "Running Sp1 prover"
-			cargo ${TOOLCHAIN_SP1} run ${FLAGS} --features sp1
+			cargo ${TOOLCHAIN_SP1} run ${PROFILE} --features sp1
 		else
 			echo "Running Sp1 tests"
-			cargo ${TOOLCHAIN_SP1} test ${FLAGS} --lib sp1-driver --features sp1 -- run_unittest_elf
-			cargo ${TOOLCHAIN_SP1} test ${FLAGS} -p raiko-host -p sp1-driver --features "sp1 enable"
+			cargo ${TOOLCHAIN_SP1} test ${PROFILE} --lib sp1-driver --features sp1 -- run_unittest_elf
+			cargo ${TOOLCHAIN_SP1} test ${PROFILE} -p raiko-host -p sp1-driver --features "sp1 enable"
 		fi
 	fi
 fi
