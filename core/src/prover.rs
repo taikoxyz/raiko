90202659dde36cae06330690fb81ae9896ad31eb
CeciliaZ030 <zhangyixin319@gmail.com>
2024-08-25 11:41:56 +0000

    update

--- core/src/prover.rs
+++ core/src/prover.rs
@@ -84,9 +84,9 @@ async fn test_native_prover() {
                 None
             }
         })
-        .unwrap_or_else(|| std::path::PathBuf::from(DATA).join("input-taiko_mainnet-182300.json"));
+        .unwrap_or_else(|| std::path::PathBuf::from(DATA).join("input-taiko_mainnet-292300.json"));
     println!("Reading GuestInput from {:?}", path);
-    let json = std::fs::read_to_string("input-taiko_mainnet-182300.json").unwrap();
+    let json = std::fs::read_to_string("input-taiko_mainnet-292300.json").unwrap();
 
     // Deserialize the input.
     let input: GuestInput = serde_json::from_str(&json).unwrap();
