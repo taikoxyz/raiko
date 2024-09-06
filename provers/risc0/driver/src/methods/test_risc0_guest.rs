21c97cb176479602f0c0eb0ba6e41038464ec1c0
smtmfft <99081233+smtmfft@users.noreply.github.com>
2024-09-06 15:09:28 +0800

    debg CI (#359)
    * debg CI
    
    Signed-off-by: smtmfft <smtm@taiko.xyz>
    
    * switch reth back to v1.0.0-rc.2-taiko branch
    
    Signed-off-by: smtmfft <smtm@taiko.xyz>
    
    ---------
    
    Signed-off-by: smtmfft <smtm@taiko.xyz>

--- provers/risc0/driver/src/methods/test_risc0_guest.rs
+++ provers/risc0/driver/src/methods/test_risc0_guest.rs
@@ -1,6 +1,6 @@
 pub const TEST_RISC0_GUEST_ELF: &[u8] = include_bytes!(
-    "../../../guest/target/riscv32im-risc0-zkvm-elf/release/deps/risc0_guest-f5987cc5887deb20"
+    "../../../guest/target/riscv32im-risc0-zkvm-elf/release/deps/risc0_guest-4ff36f602a7f8785"
 );
 pub const TEST_RISC0_GUEST_ID: [u32; 8] = [
-    2430015116, 3517447033, 2144906779, 1977372976, 3657453511, 3589121667, 1749151289, 1572046207,
+    487317851, 25800719, 3284824055, 1294483162, 678500656, 385631844, 1177387042, 72350148,
 ];
