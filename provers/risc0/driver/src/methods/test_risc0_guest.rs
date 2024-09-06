a0d8a74b9ea36fccad9030d2cad4d3f3a5e5ac80
smtmfft <smtm@taiko.xyz>
2024-09-06 09:17:13 +0800

    update prebuilt image
    Signed-off-by: smtmfft <smtm@taiko.xyz>

--- provers/risc0/driver/src/methods/test_risc0_guest.rs
+++ provers/risc0/driver/src/methods/test_risc0_guest.rs
@@ -2,5 +2,5 @@ pub const TEST_RISC0_GUEST_ELF: &[u8] = include_bytes!(
     "../../../guest/target/riscv32im-risc0-zkvm-elf/release/deps/risc0_guest-90b3fe531b061f6a"
 );
 pub const TEST_RISC0_GUEST_ID: [u32; 8] = [
-    3314962145, 673773500, 3438363205, 2146189104, 683071622, 2980760450, 628816993, 4236382820,
+    3252456902, 53901671, 541073573, 102077000, 3257334206, 588733068, 3223781439, 2047063023,
 ];
