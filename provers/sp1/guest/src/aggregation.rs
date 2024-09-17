2e6f7d8491b4cd84e87b828caa397f8eaa8ca146
Petar Vujovic <petarvujovic98@gmail.com>
2024-09-17 16:37:29 +0200

    fix(sp1): enable verification for proof aggregation

--- provers/sp1/guest/src/aggregation.rs
+++ provers/sp1/guest/src/aggregation.rs
@@ -1,15 +1,14 @@
 //! Aggregates multiple block proofs
-
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use sha2::Sha256;
-use sha2::Digest;
+use sha2::{Digest, Sha256};
 
-use raiko_lib::protocol_instance::words_to_bytes_le;
-use raiko_lib::protocol_instance::aggregation_output;
-use raiko_lib::input::ZkAggregationGuestInput;
-use raiko_lib::primitives::B256;
+use raiko_lib::{
+    input::ZkAggregationGuestInput,
+    primitives::B256,
+    protocol_instance::{aggregation_output, words_to_bytes_le},
+};
 
 pub fn main() {
     // Read the aggregation input
@@ -17,9 +16,16 @@ pub fn main() {
 
     // Verify the block proofs.
     for block_input in input.block_inputs.iter() {
-        sp1_zkvm::lib::verify::verify_sp1_proof(&input.image_id, &Sha256::digest(block_input).into());
+        sp1_zkvm::lib::verify::verify_sp1_proof(
+            &input.image_id,
+            &Sha256::digest(block_input).into(),
+        );
     }
 
     // The aggregation output
-    sp1_zkvm::io::commit_slice(&aggregation_output(B256::from(words_to_bytes_le(&input.image_id)), input.block_inputs));
+    sp1_zkvm::io::commit_slice(&aggregation_output(
+        B256::from(words_to_bytes_le(&input.image_id)),
+        input.block_inputs,
+    ));
 }
+
