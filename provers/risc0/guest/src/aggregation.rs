247ffee77afe086466063f2b00d39b68daf65e7b
Petar Vujovic <petarvujovic98@gmail.com>
2024-09-17 16:37:05 +0200

    refactor(risc0): clean up aggregation file

--- provers/risc0/guest/src/aggregation.rs
+++ provers/risc0/guest/src/aggregation.rs
@@ -1,14 +1,18 @@
+//! Aggregates multiple block proofs
 #![no_main]
 harness::entrypoint!(main);
-use risc0_zkvm::{serde, guest::env};
-use raiko_lib::protocol_instance::words_to_bytes_le;
-use raiko_lib::protocol_instance::aggregation_output;
-use raiko_lib::input::ZkAggregationGuestInput;
-use raiko_lib::primitives::B256;
 
-fn main() {
+use risc0_zkvm::{guest::env, serde};
+
+use raiko_lib::{
+    input::ZkAggregationGuestInput,
+    primitives::B256,
+    protocol_instance::{aggregation_output, words_to_bytes_le},
+};
+
+pub fn main() {
     // Read the aggregation input
-    let input: ZkAggregationGuestInput = env::read();
+    let input = env::read::<ZkAggregationGuestInput>();
 
     // Verify the proofs.
     for block_input in input.block_inputs.iter() {
@@ -17,5 +21,8 @@ fn main() {
     }
 
     // The aggregation output
-    env::commit(&aggregation_output(B256::from(words_to_bytes_le(&input.image_id)), input.block_inputs));
+    env::commit(&aggregation_output(
+        B256::from(words_to_bytes_le(&input.image_id)),
+        input.block_inputs,
+    ));
 }
