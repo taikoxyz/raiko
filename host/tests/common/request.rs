c42a53f0c9c6bb027ff143e8263013ad30df1099
keroroxx520@gmail.com <keroroxx520@gmail.com>
2024-12-09 17:19:28 +0800

    feat(host/tests): rewrite common utils and test cases

---
+++ host/tests/common/request.rs
@@ -0,0 +1,98 @@
+use raiko_core::interfaces::{ProofRequestOpt, ProverSpecificOpts};
+use raiko_host::server::api;
+use raiko_lib::consts::Network;
+use raiko_lib::proof_type::ProofType;
+use raiko_tasks::{TaskReport, TaskStatus};
+use serde_json::json;
+
+use crate::common::Client;
+
+pub fn make_proof_request(
+    network: Network,
+    proof_type: ProofType,
+    block_number: u64,
+) -> ProofRequestOpt {
+    let json_guest_input = format!(
+        "test_{}_{}_{}_{}.json",
+        network,
+        proof_type,
+        block_number,
+        std::time::Instant::now().elapsed().as_secs()
+    );
+    ProofRequestOpt {
+        block_number: Some(block_number),
+        network: Some(network.to_string()),
+        proof_type: Some(proof_type.to_string()),
+
+        // Untesting parameters
+        l1_inclusion_block_number: None,
+        l1_network: Some("ethereum".to_string()),
+        graffiti: Some(
+            "8008500000000000000000000000000000000000000000000000000000000000".to_owned(),
+        ),
+        prover: Some("0x70997970C51812dc3A010C7d01b50e0d17dc79C8".to_owned()),
+        blob_proof_type: Some("proof_of_equivalence".to_string()),
+        prover_args: ProverSpecificOpts {
+            native: Some(json!({
+                "json_guest_input": json_guest_input,
+            })),
+            sgx: None,
+            sp1: None,
+            risc0: None,
+        },
+    }
+}
+
+pub async fn complete_proof_request(api_version: &str, client: &Client, request: &ProofRequestOpt) {
+    match api_version {
+        "v2" => v2_complete_proof_request(client, request).await,
+        _ => unreachable!(),
+    }
+}
+
+pub async fn v2_complete_proof_request(client: &Client, request: &ProofRequestOpt) {
+    let start_time = std::time::Instant::now();
+    let mut interval = tokio::time::interval(std::time::Duration::from_millis(100));
+    while start_time.elapsed().as_secs() < 60 * 60 {
+        interval.tick().await;
+
+        match client
+            .post("/v2/proof", request)
+            .await
+            .expect("failed to send request")
+        {
+            // Proof generation is in progress
+            api::v2::Status::Ok {
+                data: api::v2::ProofResponse::Status { status, .. },
+            } => {
+                assert!(
+                    matches!(status, TaskStatus::Registered | TaskStatus::WorkInProgress),
+                    "status should be either Registered or WorkInProgress, got: {status:?}"
+                );
+            }
+
+            // Proof generation is successfully completed
+            api::v2::Status::Ok {
+                data: api::v2::ProofResponse::Proof { proof },
+            } => {
+                println!("proof generation completed, proof: {}", json!(proof));
+                return;
+            }
+
+            // Proof generation failed
+            api::v2::Status::Error { message, error } => {
+                panic!("proof generation failed, message: {message}, error: {error:?}");
+            }
+        }
+    }
+    panic!("proof generation failed, error: timeout");
+}
+
+/// Assert that the report is in the expected format.
+pub async fn v2_assert_report(client: &Client) -> Vec<TaskReport> {
+    let response = client
+        .get(&format!("/v2/proof/report"))
+        .await
+        .expect("failed to send request");
+    response.json().await.expect("failed to decode report body")
+}
---
+++ host/tests/common/server.rs
@@ -0,0 +1,86 @@
+use crate::common::Client;
+use raiko_host::{server::serve, Opts, ProverState};
+use rand::Rng;
+use tokio_util::sync::CancellationToken;
+
+/// Builder for a test server.
+///
+/// This builder only supports setting a few parameters for testing.
+///
+/// Examples:
+/// ```
+/// let server = TestServerBuilder::default()
+///     .port(8080)
+///     .build();
+/// ```
+#[derive(Default, Debug)]
+pub struct TestServerBuilder {
+    port: Option<u16>,
+}
+
+impl TestServerBuilder {
+    pub fn port(mut self, port: u16) -> Self {
+        self.port = Some(port);
+        self
+    }
+
+    pub fn build(self) -> TestServerHandle {
+        let port = self
+            .port
+            .unwrap_or(rand::thread_rng().gen_range(1024..65535));
+        let address = format!("127.0.0.1:{port}");
+
+        // TODO
+        // opts.config_path
+        // opts.chain_spec_path
+        let log_level = std::env::var("RUST_LOG").unwrap_or("info".to_string());
+        let opts = Opts {
+            address,
+            log_level,
+
+            concurrency_limit: 16,
+            ..Default::default()
+        };
+        let state = ProverState::init_with_opts(opts).expect("Failed to initialize prover state");
+        let token = CancellationToken::new();
+
+        // Run the server in a separate thread with the ability to cancel it when our testing is done.
+        let (state_, token_) = (state.clone(), token.clone());
+        tokio::spawn(async move {
+            tokio::select! {
+                _ = token_.cancelled() => {
+                    println!("Test done");
+                }
+                result = serve(state_) => {
+                    match result {
+                        Ok(()) => {
+                            assert!(false, "Unexpected server shutdown");
+                        }
+                        Err(error) => {
+                            assert!(false, "Server failed due to: {error:?}");
+                        }
+                    };
+                }
+            }
+        });
+
+        TestServerHandle {
+            state,
+            _token: token,
+        }
+    }
+}
+
+/// Handle for a test server.
+///
+/// Note that we don't need to cancel the server explicitly, as it will be cancelled when the token is dropped.
+pub struct TestServerHandle {
+    state: ProverState,
+    _token: CancellationToken,
+}
+
+impl TestServerHandle {
+    pub fn get_client(&self) -> Client {
+        Client::new(format!("http://{}", self.state.opts.address))
+    }
+}
