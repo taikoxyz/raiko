3433e9b54f538c0db268aec1c070efedf16df926
keroroxx520@gmail.com <keroroxx520@gmail.com>
2024-12-19 15:54:34 +0800

    feat(provers/risc0/driver): add --feature "test-mock-guest"

---
+++ provers/risc0/driver/src/methods/risc0_guest_mock.rs
@@ -0,0 +1,3 @@
+
+pub const RISC0_GUEST_MOCK_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest-mock");
+pub const RISC0_GUEST_MOCK_ID: [u32; 8] = [3661074503, 456244050, 2356448233, 3555754962, 3799155047, 1867550507, 1560708289, 2016241489];
