9a243c00b8173cea5c211f76b696a45c0f5630c4
Kero <keroroxx520@gmail.com>
2025-01-22 10:40:29 +0800

    feat: impl reqpool (#447)
    * feat: impl reqpool
    
    * feat(reqpool): mock pool
    
    * feat(reqpool): remove Pool trait
    
    * feat(reqpool): remove memory pool
    
    * test(reqpool): add case for multiple redis pools
    
    * chore: allow unused_imports
    
    * feat(reqpool): update Cargo.toml
    
    * chore(reqpool): impl Display for Status
    
    * feat(reqpool): remove feature "enable-mock"

---
+++ reqpool/src/config.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+/// The configuration for the redis-backend request pool
+pub struct RedisPoolConfig {
+    /// The URL of the Redis database, e.g. "redis://localhost:6379"
+    pub redis_url: String,
+    /// The TTL of the Redis database
+    pub redis_ttl: u64,
+}
