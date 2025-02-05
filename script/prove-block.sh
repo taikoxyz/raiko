a48e92cf38377e69486fa3ab3ee11ca220c852f5
Austin Abell <austinabell8@gmail.com>
2025-02-05 15:50:51 -0500

    update po2 for testing

--- script/prove-block.sh
+++ script/prove-block.sh
@@ -104,7 +104,7 @@ elif [ "$proof" == "risc0" ]; then
         "bonsai": false,
         "snark": false,
         "profile": true,
-        "execution_po2": 18
+        "execution_po2": 21
     }
   '
 elif [ "$proof" == "risc0-bonsai" ]; then
@@ -115,7 +115,7 @@ elif [ "$proof" == "risc0-bonsai" ]; then
         "bonsai": true,
         "snark": true,
         "profile": false,
-        "execution_po2": 20
+        "execution_po2": 21
     }
   '
 else
