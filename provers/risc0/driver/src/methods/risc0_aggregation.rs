dbac6a0ee386e7fae8ad136c312fd19b6d1a352f
keroroxx520@gmail.com <keroroxx520@gmail.com>
2025-02-14 14:45:54 +0800

    provers: update guest image

--- provers/risc0/driver/src/methods/risc0_aggregation.rs
+++ provers/risc0/driver/src/methods/risc0_aggregation.rs
@@ -1,5 +1,3 @@
-pub const RISC0_AGGREGATION_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation");
-pub const RISC0_AGGREGATION_ID: [u32; 8] = [
-    3538803209, 3767093287, 2369422979, 1067348334, 3363830341, 2169933979, 1726073639, 3928900920,
-];
+
+pub const RISC0_AGGREGATION_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation");
+pub const RISC0_AGGREGATION_ID: [u32; 8] = [3538803209, 3767093287, 2369422979, 1067348334, 3363830341, 2169933979, 1726073639, 3928900920];
