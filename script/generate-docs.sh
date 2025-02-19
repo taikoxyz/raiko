b6a91547f6814ee081fd7a62ab6b02ba3508e9dd
Kero <keroroxx520@gmail.com>
2025-02-19 17:40:11 +0800

    chore: clean deprecated code (#457)
    * chore: remove deprecated code
    
    * feat(provers): bump risc0 guest image
    
    * feat(.github): temp remove sp1 ci

--- script/generate-docs.sh
+++ script/generate-docs.sh
@@ -2,9 +2,7 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-TASKDB=${TASKDB:-raiko-tasks/in-memory}
-
 cd $DIR
 
 mkdir ../openapi
-cargo run -F ${TASKDB} --bin docs >../openapi/index.html
+cargo run --bin docs >../openapi/index.html
