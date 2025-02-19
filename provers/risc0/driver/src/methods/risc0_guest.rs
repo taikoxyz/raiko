296f70fe77711a1f2c970c7c780e3bfb467a5009
keroroxx520@gmail.com <keroroxx520@gmail.com>
2025-02-19 14:20:34 +0800

    feat(provers/{risc0,sp1}/guest): bump guest image

--- provers/risc0/driver/src/methods/risc0_guest.rs
+++ provers/risc0/driver/src/methods/risc0_guest.rs
@@ -1,3 +1,5 @@
-
-pub const RISC0_GUEST_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
-pub const RISC0_GUEST_ID: [u32; 8] = [1374625060, 2023300513, 11488720, 2138063656, 3621518210, 2957239418, 2839758755, 1437183388];
+pub const RISC0_GUEST_ELF: &[u8] =
+    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-guest");
+pub const RISC0_GUEST_ID: [u32; 8] = [
+    2553091988, 937031947, 2407901877, 2652153187, 1804062976, 3314958911, 60783236, 3056307679,
+];
