cce831501ea50a3933e7c44e2da2389c49689708
keroroxx520@gmail.com <keroroxx520@gmail.com>
2025-02-19 15:00:11 +0800

    feat(provers): bump risc0 guest image

--- provers/risc0/driver/src/methods/risc0_aggregation.rs
+++ provers/risc0/driver/src/methods/risc0_aggregation.rs
@@ -1,3 +1,5 @@
-
-pub const RISC0_AGGREGATION_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation");
-pub const RISC0_AGGREGATION_ID: [u32; 8] = [4022754202, 1011421267, 2640631431, 1070713652, 4010487621, 3530055996, 68277198, 2146180808];
+pub const RISC0_AGGREGATION_ELF: &[u8] =
+    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation");
+pub const RISC0_AGGREGATION_ID: [u32; 8] = [
+    4022754202, 1011421267, 2640631431, 1070713652, 4010487621, 3530055996, 68277198, 2146180808,
+];
