6cddcd6c5f4c31e625aef0836d0c253e56b921cc
keroroxx520@gmail.com <keroroxx520@gmail.com>
2025-03-11 18:30:34 +0800

    feat(reqactor): action.is_high_priority() based on the action kind

--- reqactor/src/action.rs
+++ reqactor/src/action.rs
@@ -15,11 +15,19 @@ pub enum Action {
 }
 
 impl Action {
+    /// Get the request key of the action.
     pub fn request_key(&self) -> &RequestKey {
         match self {
             Action::Prove { request_key, .. } => request_key,
             Action::Cancel { request_key, .. } => request_key,
         }
+    }
+
+    /// Whether the action is high priority.
+    ///
+    /// Currently, only aggregation requests are considered high priority.
+    pub fn is_high_priority(&self) -> bool {
+        matches!(self, Action::Prove { request_key, .. } if matches!(request_key, RequestKey::Aggregation(_)))
     }
 }
 
