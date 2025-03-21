b39d942c675fc2c8e42950e27d966ff17da606ba
smtmfft <smtm@taiko.xyz>
2025-03-21 21:34:27 +0800

    update risc0 image
    Signed-off-by: smtmfft <smtm@taiko.xyz>

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -141,7 +141,10 @@ impl Raiko {
     }
 
     pub fn get_batch_output(&self, batch_input: &GuestBatchInput) -> RaikoResult<GuestBatchOutput> {
-        info!("Generating output for batch id: {}", batch_input.taiko.batch_id);
+        info!(
+            "Generating output for batch id: {}",
+            batch_input.taiko.batch_id
+        );
         let pool_txs_list = generate_transactions_for_batch_blocks(&batch_input.taiko);
         let blocks = batch_input.inputs.iter().zip(pool_txs_list).try_fold(
             Vec::new(),
