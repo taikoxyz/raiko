f47dc26f599e7d24897a7b1652bd4d21eef63ba5
smtmfft <smtm@taiko.xyz>
2025-03-22 16:32:24 +0800

    fix CI sp1 build and test

--- harness/macro/src/lib.rs
+++ harness/macro/src/lib.rs
@@ -61,11 +61,6 @@ pub fn entrypoint(input: TokenStream) -> TokenStream {
 
     let output = if cfg!(feature = "sp1") {
         quote! {
-            // Set up a global allocator
-            use sp1_zkvm::heap::SimpleAlloc;
-            #[global_allocator]
-            static HEAP: SimpleAlloc = SimpleAlloc;
-
             #[cfg(test)]
             #tests_entry
 
