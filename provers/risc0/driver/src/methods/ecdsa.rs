583ed8ceb3a947ca7c40d9b346875bc79f6fbc27
smtmfft <smtm@taiko.xyz>
2025-04-14 15:51:01 +0800

    update risc0 binary
    Signed-off-by: smtmfft <smtm@taiko.xyz>

--- provers/risc0/driver/src/methods/ecdsa.rs
+++ provers/risc0/driver/src/methods/ecdsa.rs
@@ -1,3 +1,5 @@
-
-pub const ECDSA_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/ecdsa.bin");
-pub const ECDSA_ID: [u32; 8] = [1930164840, 4054596513, 2882493560, 1424881730, 3057661652, 3456098792, 879571237, 2097836975];
+pub const ECDSA_ELF: &[u8] =
+    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/ecdsa.bin");
+pub const ECDSA_ID: [u32; 8] = [
+    1930164840, 4054596513, 2882493560, 1424881730, 3057661652, 3456098792, 879571237, 2097836975,
+];
--- provers/risc0/driver/src/methods/risc0_batch.rs
+++ provers/risc0/driver/src/methods/risc0_batch.rs
@@ -1,3 +1,5 @@
-
-pub const RISC0_BATCH_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
-pub const RISC0_BATCH_ID: [u32; 8] = [2311302227, 2458989830, 895933296, 2987026830, 847893046, 169594526, 204636917, 3978386598];
+pub const RISC0_BATCH_ELF: &[u8] =
+    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
+pub const RISC0_BATCH_ID: [u32; 8] = [
+    2799826313, 1146822635, 4144432365, 299604400, 536823753, 1503986349, 3698813577, 2002202435,
+];
