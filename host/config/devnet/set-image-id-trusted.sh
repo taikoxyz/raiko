310da10f691f957314801e3b38a7ab15c56e12f2
Justin Chan <justin.chan@nethermind.io>
2025-05-29 19:43:33 +1000

    feat: update set image id trusted script and env example to follow raiko 1.8.0 update

--- host/config/devnet/set-image-id-trusted.sh
+++ host/config/devnet/set-image-id-trusted.sh
@@ -17,9 +17,11 @@ docker run --rm \
   -e RISC0_VERIFIER_ADDRESS="$RISC0_VERIFIER_ADDRESS" \
   -e GUEST_PROOF_IMAGE="$GUEST_PROOF_IMAGE" \
   -e AGGREGATION_PROOF_IMAGE="$AGGREGATION_PROOF_IMAGE" \
+  -e BATCH_PROOF_IMAGE="$GUEST_PROOF_IMAGE" \
   -e PRIVATE_KEY="$PRIVATE_KEY" \
   -e FORK_URL="$FORK_URL" \
   $IMAGE sh -c '
     cast send $RISC0_VERIFIER_ADDRESS "setImageIdTrusted(bytes32,bool)" $GUEST_PROOF_IMAGE true --rpc-url $FORK_URL --private-key $PRIVATE_KEY
     cast send $RISC0_VERIFIER_ADDRESS "setImageIdTrusted(bytes32,bool)" $AGGREGATION_PROOF_IMAGE true --rpc-url $FORK_URL --private-key $PRIVATE_KEY
+    cast send $RISC0_VERIFIER_ADDRESS "setImageIdTrusted(bytes32,bool)" $BATCH_PROOF_IMAGE true --rpc-url $FORK_URL --private-key $PRIVATE_KEY
   '
