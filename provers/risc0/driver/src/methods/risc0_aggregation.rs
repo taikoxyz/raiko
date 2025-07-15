ab4ce85c1fb161c02add6f0d1d4693f87baa2dcb
Yue <zhouyue360@gmail.com>
2025-07-15 13:10:23 +1000

    update risc0 to v2.2.0

--- provers/risc0/driver/src/methods/risc0_aggregation.rs
+++ provers/risc0/driver/src/methods/risc0_aggregation.rs
@@ -1,5 +1,3 @@
-pub const RISC0_AGGREGATION_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation.bin");
-pub const RISC0_AGGREGATION_ID: [u32; 8] = [
-    2925080547, 1913260016, 177527973, 3454951254, 221425645, 4133536269, 131738968, 1340386664,
-];
+
+pub const RISC0_AGGREGATION_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation.bin");
+pub const RISC0_AGGREGATION_ID: [u32; 8] = [2069105366, 323896637, 1674464, 2357629650, 1735073394, 593512183, 2686476147, 3578318550];
