8e5aedc8dc44ac56f53bfe773458ee2658eae1bd
Yue <zhouyue360@gmail.com>
2025-07-17 19:55:31 +1000

    update format

--- provers/risc0/driver/src/methods/risc0_aggregation.rs
+++ provers/risc0/driver/src/methods/risc0_aggregation.rs
@@ -1,3 +1,5 @@
-
-pub const RISC0_AGGREGATION_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation.bin");
-pub const RISC0_AGGREGATION_ID: [u32; 8] = [2069105366, 323896637, 1674464, 2357629650, 1735073394, 593512183, 2686476147, 3578318550];
+pub const RISC0_AGGREGATION_ELF: &[u8] =
+    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-aggregation.bin");
+pub const RISC0_AGGREGATION_ID: [u32; 8] = [
+    2069105366, 323896637, 1674464, 2357629650, 1735073394, 593512183, 2686476147, 3578318550,
+];
--- provers/risc0/driver/src/methods/risc0_batch.rs
+++ provers/risc0/driver/src/methods/risc0_batch.rs
@@ -1,3 +1,5 @@
-
-pub const RISC0_BATCH_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
-pub const RISC0_BATCH_ID: [u32; 8] = [1102720304, 1407132507, 3587315974, 2566767652, 1215848185, 96193079, 3976253700, 2699738778];
+pub const RISC0_BATCH_ELF: &[u8] =
+    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
+pub const RISC0_BATCH_ID: [u32; 8] = [
+    1102720304, 1407132507, 3587315974, 2566767652, 1215848185, 96193079, 3976253700, 2699738778,
+];
