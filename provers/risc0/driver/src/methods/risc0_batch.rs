2f50db375871203f88df70f64db59d0027fec4bb
Yue <zhouyue360@gmail.com>
2025-08-22 15:50:49 +1000

    update risc0 patch crates

--- provers/risc0/driver/src/methods/risc0_batch.rs
+++ provers/risc0/driver/src/methods/risc0_batch.rs
@@ -1,5 +1,3 @@
-pub const RISC0_BATCH_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
-pub const RISC0_BATCH_ID: [u32; 8] = [
-    1102720304, 1407132507, 3587315974, 2566767652, 1215848185, 96193079, 3976253700, 2699738778,
-];
+
+pub const RISC0_BATCH_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
+pub const RISC0_BATCH_ID: [u32; 8] = [4186088415, 1305451682, 2803270861, 189673463, 1801127113, 3018362973, 155160063, 2388380359];
