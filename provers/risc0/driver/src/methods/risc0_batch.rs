dcc536920f3caa359ac465b0e47c2758c4e4065f
Yue <zhouyue360@gmail.com>
2025-08-28 00:00:26 +1000

    redo zkvm build

--- provers/risc0/driver/src/methods/risc0_batch.rs
+++ provers/risc0/driver/src/methods/risc0_batch.rs
@@ -1,5 +1,3 @@
-pub const RISC0_BATCH_ELF: &[u8] =
-    include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
-pub const RISC0_BATCH_ID: [u32; 8] = [
-    4186088415, 1305451682, 2803270861, 189673463, 1801127113, 3018362973, 155160063, 2388380359,
-];
+
+pub const RISC0_BATCH_ELF: &[u8] = include_bytes!("../../../guest/target/riscv32im-risc0-zkvm-elf/release/risc0-batch.bin");
+pub const RISC0_BATCH_ID: [u32; 8] = [3959161532, 4197968696, 3872261708, 3063741683, 1676800853, 2252407633, 4216867577, 1489617401];
